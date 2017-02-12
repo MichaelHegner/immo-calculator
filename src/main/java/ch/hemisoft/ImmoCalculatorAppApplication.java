498c6e566d094bc25f74a0dc91285d4d86a9cdca
MichaelHegner <michael.hegner@hemisoft.net>
2017-02-12 17:32:13 +0100

    IMMO-CALC: Basic Project

---
+++ src/main/java/ch/hemisoft/ImmoCalculatorAppApplication.java
@@ -0,0 +1,12 @@
+package ch.hemisoft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImmoCalculatorAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImmoCalculatorAppApplication.class, args);
+	}
+}
