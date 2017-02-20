0674d7d79d6c3189473e82f1dd13004a09460e69
MichaelHegner <michael.hegner@hemisoft.net>
2017-02-20 19:30:48 +0100

    GUI: Using JQuery Validation and separate JS to separate JS-File

---
+++ src/main/resources/static/js/app.js
@@ -0,0 +1,60 @@
+$(document).ready(function() {
+	registerEventModalDialog();
+	registerEventTableRowClick();
+	handleModalDialog();
+	registerFormSubmitEvent();
+});
+			
+function registerEventModalDialog() {
+	$("#buttonNewProperty").click(function(){
+		var params = [{name:'displayEdit', value:true}];
+		window.location = './list?' + $.param(params);
+	});
+}
+
+
+function registerEventTableRowClick() {
+	$("tr.property-row").click(function(e){
+		var params = [{name:'propertyId', value:$(this).find("td:first").text()}, {name:'displayEdit', value:true}];
+		window.location = './list?' + $.param(params);
+	});
+}
+
+
+function handleModalDialog() {
+	var currentURL = $(location).attr('href');
+	var displayEdit = getParameterByName("displayEdit", currentURL);
+	
+	if(undefined !== displayEdit) {
+		if("true" === displayEdit) {
+			displayModalDialog();
+		}
+	}
+}
+
+function displayModalDialog() {
+	$("#dialogEditProperty").modal("show");
+}
+
+function registerFormSubmitEvent() {
+	$("#propertyEditForm").validate({
+		ignore: false,
+		invalidHandler: function(form, validator) {
+			var errorList = validator.errorList;
+			var numberOfErrors = $(errorList.length)[0];
+
+			if(0 < numberOfErrors) {
+				var errorPanel = $(errorList[0].element).closest(".panel-collapse");
+				$('.panel-collapse').each(function(i, panel){
+					var errorPanelId = errorPanel.attr('id');
+					var currentPanelId = $(panel).attr('id');
+					if(currentPanelId === errorPanelId) {
+						$(panel).collapse('show');
+					} else {
+						$(panel).collapse('hide');
+					}
+				});
+			}
+		}		
+	});
+}
