c2f35cbe2ed3ab935b5326f47785386d6ed2ebef
MichaelHegner <michael.hegner@hemisoft.net>
2017-03-04 19:04:50 +0100

    SECURITY: Add simple Security

---
+++ src/main/java/ch/hemisoft/immo/calc/web/controller/LoginController.java
@@ -0,0 +1,17 @@
+package ch.hemisoft.immo.calc.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import lombok.RequiredArgsConstructor;
+
+@Controller
+@RequiredArgsConstructor
+public class LoginController {
+	
+	@GetMapping(value={"", "login"})
+	public String list(ModelMap modelMap) {
+		return "/login";
+	}
+}
