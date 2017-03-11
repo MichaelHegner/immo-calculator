0e9e9b450f50b734b6e93b4277333d7825d213e8
MichaelHegner <michael.hegner@hemisoft.net>
2017-03-11 19:38:00 +0100

    GUI LOGIN: Adding Banner

--- src/main/java/ch/hemisoft/immo/calc/web/controller/LoginController.java
+++ src/main/java/ch/hemisoft/immo/calc/web/controller/LoginController.java
@@ -3,6 +3,7 @@ package ch.hemisoft.immo.calc.web.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import lombok.RequiredArgsConstructor;
 
@@ -12,6 +13,6 @@ public class LoginController {
 	
 	@GetMapping(value={"", "login"})
 	public String list(ModelMap modelMap) {
-		return "/login";
+		return "/login/login";
 	}
 }
