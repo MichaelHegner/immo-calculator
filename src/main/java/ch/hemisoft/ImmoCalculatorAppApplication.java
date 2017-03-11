a0be0267762cda1ede45f4c933a5886ce81a896f
MichaelHegner <michael.hegner@hemisoft.net>
2017-03-11 22:10:58 +0100

    DATA: Add Dummy User to be able to connect to cloud system

--- src/main/java/ch/hemisoft/ImmoCalculatorAppApplication.java
+++ src/main/java/ch/hemisoft/ImmoCalculatorAppApplication.java
@@ -1,12 +1,34 @@
 package ch.hemisoft;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Component;
+
+import ch.hemisoft.immo.calc.backend.repository.UserRepository;
+import ch.hemisoft.immo.domain.User;
 
 @SpringBootApplication
 public class ImmoCalculatorAppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ImmoCalculatorAppApplication.class, args);
+	}
+	
+	@Component
+	class ApplicationStartupRunnerOne implements CommandLineRunner {
+		@Autowired UserRepository userRepository;
+		
+	    @Override
+	    public void run(String... args) throws Exception {
+	    	if(null == userRepository.findByUserName("user")) {
+	    		String userName = "user";
+	    		String password = "password";
+				String email = "user@hemisoft.net";
+				Boolean enabled = true;
+				userRepository.save(new User(userName, password, email, enabled));
+	    	}
+	    }
 	}
 }
