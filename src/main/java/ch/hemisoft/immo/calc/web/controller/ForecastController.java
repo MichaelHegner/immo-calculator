239144aa22f1e18ed60c7f00767a117f47e97eb4
MichaelHegner <michael.hegner@hemisoft.net>
2017-05-20 13:25:14 +0200

    FORECAST: Property Selectable

--- src/main/java/ch/hemisoft/immo/calc/web/controller/ForecastController.java
+++ src/main/java/ch/hemisoft/immo/calc/web/controller/ForecastController.java
@@ -1,11 +1,12 @@
 package ch.hemisoft.immo.calc.web.controller;
 
-import java.security.Principal;
+import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import ch.hemisoft.immo.calc.business.service.ForecastService;
@@ -24,10 +25,22 @@ public class ForecastController {
 	@NonNull final PropertyService propertyService;
 	
 	@GetMapping("/list")
-	public String edit(Principal principal, ModelMap modelMap) {
+	public String list(ModelMap modelMap) {
 		List<Property> properties = propertyService.findAll();
 		List<Forecast> forecasts = forecastService.findAll(properties);
 		modelMap.addAttribute("forecast", new ForecastDto(forecasts));
+		modelMap.addAttribute("properties", properties);
+		return "forecast/list";
+	}	
+	
+	@GetMapping("/list/{propertyId}")
+	public String list(@PathVariable Long propertyId, ModelMap modelMap) {
+		Property property = propertyService.find(propertyId);
+		List<Property> properties = Arrays.asList(property);
+		List<Forecast> forecasts = forecastService.findAll(properties);
+		modelMap.addAttribute("forecast", new ForecastDto(forecasts));
+		modelMap.addAttribute("properties", propertyService.findAll());
+		modelMap.addAttribute("property", property);
 		return "forecast/list";
 	}
 }
