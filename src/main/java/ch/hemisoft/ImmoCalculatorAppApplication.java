214e5224f2a823b46132f0c8931d7a17c39a1033
MichaelHegner <michael.hegner@hemisoft.net>
2017-07-02 17:46:22 +0200

    BUGFIX: Add ForcastConfigurationIfNOtExist when starting application

--- src/main/java/ch/hemisoft/ImmoCalculatorAppApplication.java
+++ src/main/java/ch/hemisoft/ImmoCalculatorAppApplication.java
@@ -5,8 +5,11 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Component;
+import org.springframework.util.CollectionUtils;
 
+import ch.hemisoft.immo.calc.backend.repository.ForecastConfigurationRepository;
 import ch.hemisoft.immo.calc.backend.repository.UserRepository;
+import ch.hemisoft.immo.domain.ForecastConfiguration;
 import ch.hemisoft.immo.domain.User;
 
 @SpringBootApplication
@@ -19,6 +22,7 @@ public class ImmoCalculatorAppApplication {
 	@Component
 	class ApplicationStartupRunnerOne implements CommandLineRunner {
 		@Autowired UserRepository userRepository;
+		@Autowired ForecastConfigurationRepository forcastConfigurationRepository;
 		
 	    @Override
 	    public void run(String... args) throws Exception {
@@ -29,6 +33,33 @@ public class ImmoCalculatorAppApplication {
 				Boolean enabled = true;
 				userRepository.save(new User(userName, password, email, enabled));
 	    	}
+	    	
+	    	if(CollectionUtils.isEmpty(forcastConfigurationRepository.findAll())) {
+	    		forcastConfigurationRepository.save(createAustrianConfig());
+	    		forcastConfigurationRepository.save(createGermenConfig());
+	    	}
 	    }
+
+		private ForecastConfiguration createAustrianConfig() {
+			ForecastConfiguration deConfig = new ForecastConfiguration();
+			deConfig.setCountryCode("AT");
+			deConfig.setDeprecation(2.0);
+			deConfig.setRentalIncrease(5.0);
+			deConfig.setRentalIncreaseFrequence(2);
+			deConfig.setRunningCostIndex(1.5);
+			deConfig.setTaxQuote(50.0);
+			return deConfig;
+		}
+
+		private ForecastConfiguration createGermenConfig() {
+			ForecastConfiguration deConfig = new ForecastConfiguration();
+			deConfig.setCountryCode("DE");
+			deConfig.setDeprecation(2.0);
+			deConfig.setRentalIncrease(3.0);
+			deConfig.setRentalIncreaseFrequence(3);
+			deConfig.setRunningCostIndex(1.5);
+			deConfig.setTaxQuote(43.0);
+			return deConfig;
+		}
 	}
 }
