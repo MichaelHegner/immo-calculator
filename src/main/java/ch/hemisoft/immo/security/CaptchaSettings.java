54cbc4bbe60ca66ce59daecbc4676c154953a560
Michael Hegner <michaelhegner77@gmail.com>
2019-04-07 18:36:25 +0200

    SECURITY: Move Captcha information to application.properties

--- src/main/java/ch/hemisoft/immo/security/CaptchaSettings.java
+++ src/main/java/ch/hemisoft/immo/security/CaptchaSettings.java
@@ -1,13 +1,11 @@
 package ch.hemisoft.immo.security;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
 
 import lombok.Data;
 
 @Component
-@PropertySource("classpath:config/captcha.properties")
 @ConfigurationProperties(prefix = "google.recaptcha.key")
 @Data
 public class CaptchaSettings {
