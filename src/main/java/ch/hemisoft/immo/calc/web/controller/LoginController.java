a21093afb4b3d35d33956aae09150877046b0901
Michael Hegner <michaelhegner77@gmail.com>
2019-06-23 20:13:56 +0200

    BUGFIX: Spring Security Logging out when Saving

--- src/main/java/ch/hemisoft/immo/calc/web/controller/LoginController.java
+++ src/main/java/ch/hemisoft/immo/calc/web/controller/LoginController.java
@@ -2,6 +2,8 @@ package ch.hemisoft.immo.calc.web.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import lombok.RequiredArgsConstructor;
 
@@ -10,7 +12,12 @@ import lombok.RequiredArgsConstructor;
 public class LoginController {
 	
 	@GetMapping({"/", "/login"})
-	public String list() {
+	public String login() {
 		return "security/login";
+	}
+	
+	@PostMapping("/logout")
+	public ModelAndView logout() {
+		return new ModelAndView("redirect:/login?logout");
 	}
 }
